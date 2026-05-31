80ef396b734551f1ec21c59581f12b547f553c8c
archyyu <archyyu321@gmail.com>
2026-05-31 11:05:22 +0100

    delete the unused java files, make the project clean

--- src/main/java/com/archy/texasholder/repo/GameActionDBRepository.java
+++
@@ -1,9 +0,0 @@
-package com.archy.texasholder.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.archy.texasholder.entity.GameActionDB;
-
-public interface GameActionDBRepository extends JpaRepository<GameActionDB, Integer>{
-    
-}
